1c238d7b802e04f008c934b4ea15b55e022e1799
VoidOfMaya <mayaofthevoid@gmail.com>
2025-12-14 15:08:38 +0200

    refactor(click handlers):move click handling to apropriet handeling functions

--- src/component/quantity-btn/quantity.jsx
+++ src/component/quantity-btn/quantity.jsx
@@ -1,18 +1,30 @@
 import { useState } from 'react'
 import styles from './quantity.module.css'
+
 const QuantityBtn = () =>{
     const [quantity, setQuantity]= useState(1)
+
+    const onChangeHandler = (e) =>{
+        setQuantity(Number(e.target.value));
+    }
+    const handleIncrement = () =>{
+        setQuantity(prev =>{return prev + 1})
+    }
+    const handleDecremnt = () =>{
+        setQuantity(prev =>{return prev - 1})
+    }
     return(
         <div className={styles.selector}>
             <button className={styles.selectorBtn}
-                    onClick={()=>setQuantity(prev =>{return prev + 1})}>+</button>
+                    onClick={handleIncrement}>+</button>
 
             <input type="number" 
-                   placeholder={quantity}      
-                   className={styles.selectorInput}></input>
+                   value={quantity}     
+                   className={styles.selectorInput}
+                   onChange={onChangeHandler}></input>
 
             <button className={styles.selectorBtn}
-                    onClick={()=>setQuantity(prev =>{return prev + 1})}>-</button>
+                    onClick={handleDecremnt}>-</button>
         </div>
     )
 }
---
+++ src/component/quantity-btn/quantity.test.jsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { QuantityBtn } from "./quantity";
+
+describe('item quantity selector',()=>{
+    it("renders selector component")
+})
