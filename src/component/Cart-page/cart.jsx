3711fe53a6889703b72b6763618e31c85ab79bd1
VoidOfMaya <mayaofthevoid@gmail.com>
2025-12-16 14:42:39 +0200

    feat(cart-page):inject items added to cart into cart-page

--- src/component/Cart-page/cart.jsx
+++ src/component/Cart-page/cart.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useOutletContext } from "react-router"
+import style from './cart.module.css';
 
 const Cart = () =>{
     const {cartItems }= useOutletContext();
     const [items, setItems]= useState(cartItems);
 
+    const handleDisplay = () =>{
+        if (items.length > 0){
+            return items.map(item=><div>{item.name}</div>)
+        }else{
+            return<div>Cart is empty</div>
+        }
+ 
+    }
+    //<div className={style.cartContainer}>{items.length > 0 && items.map(item=> <div>{item.name}</div>)}</div>    
     return(
-         <div>You Cart is empty</div>    
+         <div className={style.cartContainer}>{handleDisplay()}</div>    
     )
 }
 export {
