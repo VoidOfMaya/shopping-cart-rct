4074637b3180529d9b062cbf2a4496e558812251
VoidOfMaya <mayaofthevoid@gmail.com>
2025-12-16 08:30:36 +0200

    feat(card):display quantity selector and add btn when hovoring over card

--- src/component/product-card/card.jsx
+++ src/component/product-card/card.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
+import { QuantityBtn } from "../quantity-btn/quantity";
 import PropType from 'prop-types';
 import style from './card.module.css';
 
 const Card = ({id, photo, name, price, info, rating}) =>{
-    const isInterested = (interest= false) =>{
+    const [hover, setHover]= useState(false)
+    const isInterested = (interest = false) =>{
         if(interest){
             console.log('entered card');
             return(
@@ -20,14 +22,20 @@ const Card = ({id, photo, name, price, info, rating}) =>{
     }
     return(
         <div className={style.card}
-             onMouseEnter={()=>isInterested(true)}
-             onMouseLeave={()=>isInterested(false)}
+             onMouseEnter={()=>setHover(true)}
+             onMouseLeave={()=>setHover(false)}
              aria-label='card-element'>
             <img src={photo} alt={name + ' photo'} className={style.photo}></img>
             <div className={style.cardInfo}>
                 <h3 className={style.name}>{name}</h3>
                 <h2 className={style.price} aria-label='product price'>{price}$</h2>
             </div>
+            {hover &&(
+                <div className={style.addToCart}>
+                    <QuantityBtn className={style.addQuantity} aria-label="Add to cart"/>
+                    <button aria-label="Add to cart">add to cart</button>
+                </div>
+            )}
         </div>
     )
 }
