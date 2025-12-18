40ea3258e46214b17e2f29826000c2490034febf
VoidOfMaya <mayaofthevoid@gmail.com>
2025-12-18 13:14:54 +0200

    feat(footer)style and center infro

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -1,24 +1,32 @@
 import style from './footer.module.css'
+import mailSvg from '/src/assets/email-fast-outline.svg'
+import phoneSvg from '/src/assets/phone (1).svg'
 const Footer =()=>{
     return(
         <footer>
-            <div className={style.footerContainer}>
-                <div className={style.brand}>© 2025 MyShop</div>
-                <div className={style.links}>
-                    <a href="#">Home</a>
-                    <a href="#">Store</a>
-                    <a href="#">Cart</a>
-                    <a href="#">About</a>
+            <div className={style.compContainer}>
+                <div className={style.footerContainer}>
+                    <h1 >Contact us</h1>
+                    <p >
+                        Ushop is a global ecommers platform!.<br/>
+                        your experience is very important to us<br/>
+                        for all enqueries please contact us</p>
+                    <div className={style.brand}>© 2025 UShop</div>
+
                 </div>
-                <div className={style.social}>
-                    <a href="#">FB</a>
-                    <a href="#">TW</a>
-                    <a href="#">IG</a>
-                </div>
-                <div className={style.cartSummary}>
-                    3 items — $75 total
+                <div>
+                    <address>
+                        <b>address:</b> 123 Random Road, Random Town,jupiter 12345<br/>
+                        <div className={style.footerContact} aria-label='Phone info:'>
+                        <img className={style.footerIcon} src={phoneSvg} alt="Phone:" /> +123456789<br/>
+                        </div>
+                        <div className={style.footerContact} aria-label='Mail info:'>
+                        <img className={style.footerIcon} src={mailSvg} alt="Mail:" /> example123@examplemail.com
+                        </div>
+                    </address>
                 </div>
             </div>
+
         </footer>
     )
 }
