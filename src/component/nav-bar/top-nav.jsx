1423414e140ca9dd51a698297b4fb9def2fc2624
VoidOfMaya <mayaofthevoid@gmail.com>
2025-12-18 14:20:20 +0200

    feat(nav)resize for small screens

--- src/component/nav-bar/top-nav.jsx
+++ src/component/nav-bar/top-nav.jsx
@@ -5,24 +5,25 @@ import homeSvg from '/src/assets/home (1).svg'
 import shopSvg from '/src/assets/shopping.svg'
 
 const TopNav = ({cart}) =>{
-    return(
-        <nav>
-            <h1 className={style.branding}>UShop</h1>
 
-            <Link to={'/'} className={style.navLink}>
-            <img className={style.navIcons} src={homeSvg} alt="Home"/>
-            Home</Link>
-            <Link to={'Store'} className={style.navLink}>
-            <img className={style.navIcons} src={shopSvg} alt="Shop"/>
-            Store</Link>
-            <Link to={'Cart'} className={style.navLink} >
-            <div className={style.cartItems}>{cart}</div>
-            <img  className={style.navIcons} src={cartDvg} alt="Cart"/>
-            
-            Cart
-            </Link>
-            
-        </nav>
+    return(
+        <div className={style.navBar}>
+            <h1 className={style.branding}>UShop</h1>
+            <nav>
+                <Link to={'/'} className={style.navLink}>
+                <img className={style.navIcons} src={homeSvg} alt="Home"/>
+                Home</Link>
+                <Link to={'Store'} className={style.navLink}>
+                <img className={style.navIcons} src={shopSvg} alt="Shop"/>
+                Store</Link>
+                <Link to={'Cart'} className={style.navLink} >
+                <div className={style.cartItems}>{cart}</div>
+                <img  className={style.navIcons} src={cartDvg} alt="Cart"/>
+                
+                Cart
+                </Link>
+            </nav>
+        </div>
     )
 }
 
