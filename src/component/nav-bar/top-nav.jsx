1f9f69f36f4622ec9cdae2a543d5830123277aae
VoidOfMaya <mayaofthevoid@gmail.com>
2025-12-18 12:24:11 +0200

    style(footer/nav)add gradient coloring

--- src/component/nav-bar/top-nav.jsx
+++ src/component/nav-bar/top-nav.jsx
@@ -1,15 +1,26 @@
 import { Link } from "react-router"
 import style from'./nav.module.css'
+import cartDvg from '/src/assets/cart-outline.svg'
+import homeSvg from '/src/assets/home (1).svg'
+import shopSvg from '/src/assets/shopping.svg'
 
 const TopNav = ({cart}) =>{
     return(
         <nav>
-            <h1 className={style.branding}>myShop</h1>
-            <Link to={'/'} className={style.navLink}>Home</Link>
-            <Link to={'Store'} className={style.navLink}>Store</Link>
+            <h1 className={style.branding}>UShop</h1>
+
+            <Link to={'/'} className={style.navLink}>
+            <img className={style.navIcons} src={homeSvg} alt="Home"/>
+            Home</Link>
+            <Link to={'Store'} className={style.navLink}>
+            <img className={style.navIcons} src={shopSvg} alt="Shop"/>
+            Store</Link>
             <Link to={'Cart'} className={style.navLink}>
             <div className={style.cartItems}>{cart}</div>
-            Cart</Link>
+
+            <img  className={style.navIcons} src={cartDvg} alt="Cart"/>
+            Cart
+            </Link>
             
         </nav>
     )
